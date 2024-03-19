b9fdeacb282b6eb3e8b2f35bc07aeccb4db54f8e
janeyanisa1234 <129841440+janeyanisa1234@users.noreply.github.com>
2024-03-19 21:08:52 +0700

    Add files via upload

---
+++ front-end/Post-item.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+const PostItem =(props)=>{
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+        </div>
+    );
+    }
+
+export default PostItem;
